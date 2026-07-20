a7897f7d16890bd9d1ff83e0a73bff06ddcdba21
Rohan Kapoor <36389262+rohakapo@users.noreply.github.com>
2026-07-20 23:20:47 +0200

    Route website pillars to precise Cal.com diagnostics

--- src/lib/calendars.ts
+++ src/lib/calendars.ts
@@ -1,7 +1,9 @@
 export const CALENDAR_URLS = {
   team: "https://cal.com/team/ravencourt-capital",
   strategicAdvisory:
-    "https://cal.com/team/ravencourt-capital/strategic-capital-investor-readiness-founders-gps",
-  operatingIntelligence: "https://cal.com/team/ravencourt-capital",
-  commercialExpansion: "https://cal.com/team/ravencourt-capital",
+    "https://cal.com/team/ravencourt-capital/private-markets-strategic-advisory-assessment",
+  operatingIntelligence:
+    "https://cal.com/team/ravencourt-capital/operating-intelligence-diagnostic",
+  commercialExpansion:
+    "https://cal.com/team/ravencourt-capital/cross-border-commercial-intelligence-diagnostic",
 } as const;
